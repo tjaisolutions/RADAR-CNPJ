c706f8c93f3c03972db4cb36aeead3c3ae6c1a14
tjaisolutions <solutionstjai@gmail.com>
2025-12-05 09:53:54 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -10,5 +11,14 @@ export default defineConfig({
     outDir: 'dist',
     emptyOutDir: true,
     sourcemap: false
+  },
+  server: {
+    proxy: {
+      '/infosimples-proxy': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
